16f64ac9801f077fd4e56838c42a955c9ed50c84
HaripriyaSathish <priyarajendren2303@gmail.com>
2026-08-04 14:48:22 +0530

    Read ALLOWED_HOSTS from environment variable

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-ll5vv^s8m73fhzi1can&2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']  # tighten this before production deploy
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 
 # Application definition
